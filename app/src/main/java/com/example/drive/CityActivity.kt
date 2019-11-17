e4235c263618f42ecb84bd9e243efb8b5bcd131e
Airat Muhutdinov <airat.mukhutdinov@simbirsoft.com>
2019-11-17 17:45:38 +0300

    create two monitor

--- app/src/main/java/com/example/drive/CityActivity.kt
+++ app/src/main/java/com/example/drive/CityActivity.kt
@@ -1,17 +1,46 @@
 package com.example.drive
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.drive.ui.login.LoginActivity
 
-class CityActivity : AppCompatActivity() {
+class CityActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_city)
 
+        val cityList = listOf("Самара", "Москва")
         val statusAuth = findViewById<TextView>(R.id.textView2)
         statusAuth.text = LoginActivity.authData?.status.toString()
+
+        val spinner = findViewById<Spinner>(R.id.cities);
+        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, cityList)
+        // Определяем разметку для использования при выборе элемента
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Применяем адаптер к элементу spinner
+        spinner.adapter = adapter
+
+        val buttonActivityMain = findViewById<Button>(R.id.choiseCity)
+        buttonActivityMain.setOnClickListener(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.choiseCity -> {
+                val intent = Intent(this, RoadProblemActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/drive/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/drive/ui/login/LoginActivity.kt
@@ -1,6 +1,5 @@
 package com.example.drive.ui.login
 
-import android.app.ListActivity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -11,28 +10,31 @@ import android.widget.EditText
 import androidx.annotation.RequiresApi
 import com.example.drive.CityActivity
 import com.example.drive.R
-import com.example.drive.requests.AuthRequest
 import com.example.drive.requests.api.AuthApi
 import com.example.drive.requests.api.AuthApiImpl
 import com.example.drive.response.AuthResponse
 import com.google.gson.GsonBuilder
-import com.google.gson.JsonObject
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.os.Handler
 
-class LoginActivity : AppCompatActivity(), View.OnClickListener {
+
+open class LoginActivity : AppCompatActivity(), View.OnClickListener {
 
 
     val retrofit = Retrofit.Builder()
         .baseUrl("http://85.236.182.178:8081")
-        .addConverterFactory(GsonConverterFactory.create(
-            GsonBuilder()
-                .setLenient()
-                .create()
-        ))
-        //.addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(
+            GsonConverterFactory.create(
+                GsonBuilder()
+                    .setLenient()
+                    .create()
+            )
+        )
         .build()
 
     companion object {
@@ -40,13 +42,11 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+        //MapKitFactory.setApiKey("3c32f6da-350f-4245-bec1-f85c14739be3")
         setContentView(R.layout.activity_login)
+        super.onCreate(savedInstanceState)
         val buttonActivityMain = findViewById<Button>(R.id.login)
         buttonActivityMain.setOnClickListener(this)
-
-
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -64,6 +64,11 @@ class LoginActivity : AppCompatActivity(), View.OnClickListener {
                 call.enqueue(AuthApiImpl())
 
                 val intent = Intent(this, CityActivity::class.java)
+                val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+                progressBar.secondaryProgress = 100
+                progressBar.progress = 10
+                progressBar.max = 100
+                progressBar.visibility = View.VISIBLE
                 Thread {
                     while (authData == null) {
                         println("Здесь у пользователя будет анимация ожидании, мы не пустим его пока не получим ответ от сервера")
